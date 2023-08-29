347f5d936f554a16236415a55be5618359aa9080
Arpit1812 <arpitdogra033@gmail.com>
2023-08-29 14:11:11 +0530

    Added Routs using node and express

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const toggle_btn      = document.querySelector(".toggle-btn")
-const toggle_btn_icon = document.querySelector(".toggle-btn i")
-const dropdown_menu   = document.querySelector(".dropdown-menu")
+const express = require("express")
+// const toggle_btn      = document.querySelector(".toggle-btn")
+// const toggle_btn_icon = document.querySelector(".toggle-btn i")
+// const dropdown_menu   = document.querySelector(".dropdown-menu")
 
-toggle_btn.addEventListener("click", () => {
-    dropdown_menu.classList.toggle("open");
+ const app = express()
+ app.use(express.static('public'));
+// toggle_btn.addEventListener("click", () => {
+//     dropdown_menu.classList.toggle("open");
 
-    toggle_btn_icon.classList = dropdown_menu.classList.contains("open")
-                                ? "fa-solid fa-xmark"
-                                : "fa-solid fa-bars"
+//     toggle_btn_icon.classList = dropdown_menu.classList.contains("open")
+//                                 ? "fa-solid fa-xmark"
+//                                 : "fa-solid fa-bars"
 
+// })
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html")
+})
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port');
 })
